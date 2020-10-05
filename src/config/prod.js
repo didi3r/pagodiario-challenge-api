30def4b1fd9b349f3705482be9087303b0970fe6
Allan Didier Perez Cortes <adpcortes@rainforrent.com>
2020-10-04 19:34:14 -0500

    Mongo server

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,5 +2,6 @@
  * Example configuration for the PROD environment
  */
 export const config = {
-  dbUrl: 'mongodb://localhost:27017/pagodiario-prod'
+  dbUrl:
+    'mongodb+srv://pagodiario:p4g0d14r10@cluster0.vmzhv.mongodb.net/pagodiario-prod'
 };
